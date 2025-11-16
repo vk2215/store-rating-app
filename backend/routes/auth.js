57936c62ad72b45ec2d59b9ceb17e4ea597de400
Vaishnavi Bharat Kale <100363871+vk2215@users.noreply.github.com>
2025-11-16 12:08:00 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { signup, login } from '../controllers/authController.js';
 
 const router = Router();
-
 router.post('/signup', signup);
 router.post('/login', login);
 
